81111dcb7653a54efa2f747c18fb7dc9a40bbcbd
Gareth Reese <gareth@foresightmobile.com>
2022-10-17 10:13:44 +0100

    Remove duplicate files and a bit more tidying

--- app/src/main/java/com/flagsmith/android/screens/ChoosePageActivity.kt
+++ app/src/main/java/com/flagsmith/android/screens/ChoosePageActivity.kt
@@ -5,10 +5,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.flag.android.screens.flag.FeatureSearchActivity
 import com.flagmsith.R
-import com.flagsmith.android.android.screens.flag.FeatureSearchActivity
-import com.flagsmith.android.android.screens.trait.TraitsActivity
 import com.flagsmith.android.screens.flag.FlagListActivity
+import com.flagsmith.android.screens.trait.TraitsActivity
 
 class ChoosePageActivity : AppCompatActivity() {
 
@@ -43,8 +43,8 @@ class ChoosePageActivity : AppCompatActivity() {
     }
 
     private fun setupBtTrait() {
-        val bt_trait : Button = findViewById(R.id.bt_trait)
-        bt_trait.setOnClickListener{
+        val btTrait : Button = findViewById(R.id.bt_trait)
+        btTrait.setOnClickListener{
 
             val i = Intent( activity, TraitsActivity::class.java)
             i.flags = Intent.FLAG_ACTIVITY_NEW_TASK
